0d8382e5415e6fb38407ea71dd5433d90fdd420f
Abande Abanga Emmanuel <116530341+Emmanuel-213@users.noreply.github.com>
2026-05-06 04:26:42 +0000

    Initialize app state with default values

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
+const app = document.getElementById("app");
 
+const state = {
+  data: [],
+  expanded: new Set(),
+  selectedFileId: null,
+  focusedId: null,
+  search: "",
+  starred: new Set(loadStarredFiles())
+};
